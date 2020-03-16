0371a26812392b6eb0275192a64543e0abb63c24
shatice <sarah.gungor@hetic.net>
2020-03-17 00:58:59 +0100

    set route /party

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   // res.render('the fish knows');
-  res.render('index', { title: 'Liste partagée' });
+  res.render('index', { title: 'Accueil' });
 });
 
-app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
+app.post('/party', (req, res) => {
+  // console.log(req.body);
+  // res.send('Post ok !');
+  axios
+  .post(`${process.env.API_URL}/party`, req.body)
+  .then(({ data }) => console.log(data))
+  .catch((err) => console.error(err));
+});
 
-app.set('view engine', 'pug');
+app.get('/party/:id', (req, res) => {
+  res.render('party', { title: 'Mon évènement' });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Front app listening on port ${process.env.PORT}!`),
+);
