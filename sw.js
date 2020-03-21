2451049e233d90c0fb05f0eb8a1aec1b11f49058
shatice <sarah.gungor@hetic.net>
2020-03-21 16:24:13 +0100

    request intercept

--- sw.js
+++ sw.js
@@ -2,3 +2,11 @@ addEventListener('install', (event) => {
   const name = 'shatice';
   console.log(`Hola ${ name }, from the sw`)
 });
+
+addEventListener('fetch', (event) => {
+  console.log(event);
+
+  if (event.request.headers.get('Accept').includes('text/html')) {
+    event.respondWith(fetch(event.request))
+  }
+});
